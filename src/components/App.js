012eac334ebd25dcea68282550adabf3500ccf06
Mukitul Islam Mukit <imukit007@gmail.com>
2018-04-07 01:12:25 +0600

    Add product card layout & styles

--- src/components/App.js
+++ src/components/App.js
@@ -11,15 +11,88 @@ class App extends Component {
             <button>Search</button>
           </div>
           <div className="search-result">
-            <p>Search result</p>
+            <div className="product-card">
+              <span className="price-tag">$11.50</span>
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/5168%2ByKCPcL._UL900_.jpg"
+                alt="bag"
+              />
+              <div className="product-card-content">
+                <h4>
+                  <b>Product Title</b>
+                </h4>
+                <button>+ Cart</button>
+              </div>
+            </div>
+            <div className="product-card">
+              <span className="price-tag">$11.50</span>
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/41eQl0UnLCL._UL900_.jpg"
+                alt="bag"
+              />
+              <div className="product-card-content">
+                <h4>
+                  <b>Product Title</b>
+                </h4>
+                <button>+ Cart</button>
+              </div>
+            </div>
+            <div className="product-card">
+              <span className="price-tag">$11.50</span>
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/41-Pnu0n0JL._UL900_.jpg"
+                alt="bag"
+              />
+              <div className="product-card-content">
+                <h4>
+                  <b>Product Title</b>
+                </h4>
+                <button>+ Cart</button>
+              </div>
+            </div>
+            <div className="product-card">
+              <span className="price-tag">$11.50</span>
+              <img
+                src="https://images-na.ssl-images-amazon.com/images/I/5168%2ByKCPcL._UL900_.jpg"
+                alt="bag"
+              />
+              <div className="product-card-content">
+                <h4>
+                  <b>Product Title</b>
+                </h4>
+                <button>+ Cart</button>
+              </div>
+            </div>
+
+            {/* <div className="search-result-empty">
+             <p>What'll you buy today?</p> */}
           </div>
         </section>
         <section className="sidebar-section">
           <div className="sidebar-header">
-            <p>Cart</p>
+            <p>
+              <span className="sidebar-title">Cart</span>
+              <span style={{ float: 'right' }}>
+                <button disabled>Clear</button>
+              </span>
+            </p>
           </div>
-          <div className="sidebar-content">
-            <p>Product List</p>
+          {/* <div className="sidebar-content-list">
+            <table>
+              <tbody>
+                <tr>
+                  <td>1. Product 1</td>
+                  <td>$11.50</td>
+                </tr>
+                <tr>
+                  <td>2. Product 2</td>
+                  <td>$11.50</td>
+                </tr>
+              </tbody>
+            </table> */}
+
+          <div className="sidebar-content-empty">
+            <p>You have no item on your cart</p>
           </div>
         </section>
       </section>
